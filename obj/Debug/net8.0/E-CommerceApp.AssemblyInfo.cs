cdfe8627679fab89230364de08d57758f5e341d3
Fady <fadynagy147@gmail.com>
2025-10-03 14:02:03 +0300

    Documentation 2

--- obj/Debug/net8.0/E-CommerceApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-CommerceApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-CommerceApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13ec64e145bd400e0354c44902f61740003c65f2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8092a6749a98cc517936b07d8640ec4563e24c28")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-CommerceApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-CommerceApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
